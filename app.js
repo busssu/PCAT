3f8c75afe14609d161ac182f74914f0aa083a501
busssu <busraabiner@gmail.com>
2021-10-28 12:44:55 +0300

    pcat created

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+var methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
+const photoController = require('./controllers/photoControllers')
+const pageController = require('./controllers/pageController')
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
-});
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-app.get('/add', (req, res) => {
-    res.render('add');
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+
 
 const port = 3000;
 app.listen(port, () => {
